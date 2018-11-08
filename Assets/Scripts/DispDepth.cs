455df3309be2071832c08a02526a8832ffb56659
DESKTOP-23JQ893\Takaya <bumpofchicken344@gmail.com>
2018-11-09 02:11:42 +0900

    add light intensity change

--- Assets/Scripts/DispDepth.cs
+++ Assets/Scripts/DispDepth.cs
@@ -1,39 +1,58 @@
 ﻿using System.Collections;
 using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 public class DispDepth : MonoBehaviour
 {
     [SerializeField] Material _depthMat;    //デプス表示用マテリアル
+    [SerializeField] SetScene _setScene;
+
     const string SCREENSHOT_PATH = "C:/Unity/Dataset/ScreenShots/"; //スクリーンショットの保存先
     const string DEPTH_IMG_PATH = "C:/Unity/Dataset/DepthImg/";  //デプス画像の保存先
+    const string EXTENSION = ".png";
 
     void Start()
     {
         GetComponent<Camera>().depthTextureMode |= DepthTextureMode.Depth;  //カメラがデプステクスチャを生成するモード
-        StartCoroutine(CaptureCoroutine());
+        
+        StartCoroutine(CaptureScreenshot());
+        //StartCoroutine(CaptureDepth());
     }
-    
+
+    //スクリーンショットを保存
     //ReadPixelsはWaitForEndOfFrameのあとで実行しなければいけないのでコルーチンで実行 https://qiita.com/su10/items/a8f3f825155835de3d2a
-    IEnumerator CaptureCoroutine()
+    IEnumerator CaptureScreenshot()
+    {
+        for (int i = 0; i < 10; i++) {
+            var texture = new Texture2D(Screen.width, Screen.height, TextureFormat.ARGB32, false);
+            yield return new WaitForEndOfFrame();
+
+            texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+            texture.Apply();
+            var bytes = texture.EncodeToPNG();
+            File.WriteAllBytes(SCREENSHOT_PATH + i + EXTENSION, bytes);
+
+            _setScene.Set();
+            //if (i == 9) {
+            //    Graphics.Blit(texture, null, _depthMat);    //ポストエフェクトでデプス画像に変換する
+            //    yield return StartCoroutine(CaptureDepth());
+            //}
+            Destroy(texture);
+        }
+        EditorApplication.isPlaying = false;    //エディタ再生終了
+    }
+
+    //デプス画像を保存
+    IEnumerator CaptureDepth()
     {
         var texture = new Texture2D(Screen.width, Screen.height, TextureFormat.ARGB32, false);
-
         yield return new WaitForEndOfFrame();
 
-        //スクリーンショットを保存
         texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
         texture.Apply();
         var bytes = texture.EncodeToPNG();
-        File.WriteAllBytes(SCREENSHOT_PATH + "1.png", bytes);
-
-        Graphics.Blit(texture, null, _depthMat);    //ポストエフェクトでデプス画像に変換する
-
-        //デプス画像を保存
-        texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-        texture.Apply();
-        bytes = texture.EncodeToPNG();
         Destroy(texture);
-        File.WriteAllBytes(DEPTH_IMG_PATH + "1.png", bytes);
+        File.WriteAllBytes(DEPTH_IMG_PATH + "1" + EXTENSION, bytes);
     }
 }
--- Assets/Scripts/SetScene.cs
+++ Assets/Scripts/SetScene.cs
@@ -1,18 +1,19 @@
 ﻿using UnityEngine;
 
+/// <summary>シーンの各パラメータを変更する</summary>
 public class SetScene : MonoBehaviour
 {
     [SerializeField] GameObject _plane;
-
-    void Start ()
+    [SerializeField] Light _light;
+    
+    public void Set()
     {
-        //床の明るさ(V)をランダムに変更　http://nn-hokuson.hatenablog.com/entry/2017/04/12/194631
+        _light.intensity = Random.Range(3f, 8f);
+        
+        //床の明るさ(HSVのVのみ)をランダムに変更　http://nn-hokuson.hatenablog.com/entry/2017/04/12/194631
         var value = Random.Range(30f, 100f);
         _plane.GetComponent<Renderer>().material.color = Color.HSVToRGB(0f, 0f, value/255f);
+
+        Debug.Log($"Light Intensity: {_light.intensity}\nPlane Value: {value}");
     }
-	
-	void Update () 
-	{
-		
-	}
 }
